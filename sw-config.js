73ad08c43159b109be3d6805c8202a5c7b5d6f19
alexylem <alexandre.mely@gmail.com>
2026-01-12 09:58:58 +0100

    Refactor: Load Firebase SDK from CDN instead of local

--- sw-config.js
+++ sw-config.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = `v1.0.1-${new Date().getTime()}`;
+const CACHE_VERSION = `v1.0.2-${new Date().getTime()}`;
 
 const FILES_TO_CACHE = [
   'index.html',
@@ -6,8 +6,5 @@ const FILES_TO_CACHE = [
   'js/alpine.min.js',
   'js/main.js',
   'manifest.json',
-  'img/icons/icon-512x512.svg',
-  'firebase/firebase-app-compat.js',
-  'firebase/firebase-auth-compat.js',
-  'firebase/firebase-firestore-compat.js'
+  'img/icons/icon-512x512.svg'
 ];
