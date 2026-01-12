297f01e8871f1d6dbb46945f013add408e4e2a13
alexylem <alexandre.mely@gmail.com>
2026-01-12 09:05:53 +0100

    Initial commit of hometab project

---
+++ sw-config.js
@@ -0,0 +1,14 @@
+const CACHE_VERSION = 'v1.0.0-1704804109000'; // Timestamp: 2026-01-09T12:41:49.000Z
+
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/alpine.min.js',
+  '/js/main.js',
+  '/manifest.json',
+  '/img/icons/icon-512x512.svg',
+  '/firebase/firebase-app-compat.js',
+  '/firebase/firebase-auth-compat.js',
+  '/firebase/firebase-firestore-compat.js'
+];
